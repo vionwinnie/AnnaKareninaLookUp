222f7665162c92b7ece79b526649478cf5c7cd02
vionwinnie <myeung@middlebury.edu>
2016-11-29 15:18:42 +0800

    Create InvertedIndex.py

---
+++ InvertedIndex.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+
+import time 
+start = time.time()
+
+from os.path import join
+#reading all the chapters into list, clean the chapter name, concat into one string
+mypath = r"P:\Coursera\AnnaKarenina"
+
+number = list(range(0,238))
+number = [str(f)+".txt" for f in number]
+number = [join(mypath,f) for f in number]          
+book = []
+for files in number:
+    myfile = open(files,'r')
+    data = myfile.read()
+    data = data[10:]
+    book.append(data.replace('\n', ' '))
+
+
+#setting up different cleaning and parsing functions 
+_WORD_MIN_LENGTH = 3
+_STOP_WORDS = frozenset([
+'a', 'about', 'above', 'above', 'across', 'after', 'afterwards', 'again', 
+'against', 'all', 'almost', 'alone', 'along', 'already', 'also','although',
+'always','am','among', 'amongst', 'amoungst', 'amount',  'an', 'and', 'another',
+'any','anyhow','anyone','anything','anyway', 'anywhere', 'are', 'around', 'as',
+'at', 'back','be','became', 'because','become','becomes', 'becoming', 'been', 
+'before', 'beforehand', 'behind', 'being', 'below', 'beside', 'besides', 
+'between', 'beyond', 'bill', 'both', 'bottom','but', 'by', 'call', 'can', 
+'cannot', 'cant', 'co', 'con', 'could', 'couldnt', 'cry', 'de', 'describe', 
+'detail', 'do', 'done', 'down', 'due', 'during', 'each', 'eg', 'eight', 
+'either', 'eleven','else', 'elsewhere', 'empty', 'enough', 'etc', 'even', 
+'ever', 'every', 'everyone', 'everything', 'everywhere', 'except', 'few', 
+'fifteen', 'fify', 'fill', 'find', 'fire', 'first', 'five', 'for', 'former', 
+'formerly', 'forty', 'found', 'four', 'from', 'front', 'full', 'further', 'get',
+'give', 'go', 'had', 'has', 'hasnt', 'have', 'he', 'hence', 'her', 'here', 
+'hereafter', 'hereby', 'herein', 'hereupon', 'hers', 'herself', 'him', 
+'himself', 'his', 'how', 'however', 'hundred', 'ie', 'if', 'in', 'inc', 
+'indeed', 'interest', 'into', 'is', 'it', 'its', 'itself', 'keep', 'last', 
+'latter', 'latterly', 'least', 'less', 'ltd', 'made', 'many', 'may', 'me', 
+'meanwhile', 'might', 'mill', 'mine', 'more', 'moreover', 'most', 'mostly', 
+'move', 'much', 'must', 'my', 'myself', 'name', 'namely', 'neither', 'never', 
+'nevertheless', 'next', 'nine', 'no', 'nobody', 'none', 'noone', 'nor', 'not', 
+'nothing', 'now', 'nowhere', 'of', 'off', 'often', 'on', 'once', 'one', 'only',
+'onto', 'or', 'other', 'others', 'otherwise', 'our', 'ours', 'ourselves', 'out',
+'over', 'own','part', 'per', 'perhaps', 'please', 'put', 'rather', 're', 'same',
+'see', 'seem', 'seemed', 'seeming', 'seems', 'serious', 'several', 'she', 
+'should', 'show', 'side', 'since', 'sincere', 'six', 'sixty', 'so', 'some', 
+'somehow', 'someone', 'something', 'sometime', 'sometimes', 'somewhere', 
+'still', 'such', 'system', 'take', 'ten', 'than', 'that', 'the', 'their', 
+'them', 'themselves', 'then', 'thence', 'there', 'thereafter', 'thereby', 
+'therefore', 'therein', 'thereupon', 'these', 'they', 'thickv', 'thin', 'third',
+'this', 'those', 'though', 'three', 'through', 'throughout', 'thru', 'thus', 
+'to', 'together', 'too', 'top', 'toward', 'towards', 'twelve', 'twenty', 'two', 
+'un', 'under', 'until', 'up', 'upon', 'us', 'very', 'via', 'was', 'we', 'well', 
+'were', 'what', 'whatever', 'when', 'whence', 'whenever', 'where', 'whereafter',
+'whereas', 'whereby', 'wherein', 'whereupon', 'wherever', 'whether', 'which', 
+'while', 'whither', 'who', 'whoever', 'whole', 'whom', 'whose', 'why', 'will', 
+'with', 'within', 'without', 'would', 'yet', 'you', 'your', 'yours', 'yourself',
+'yourselves', 'the'])
+
+def word_split(text):
+    """
+    Split a text in words. Returns a list of tuple that contains
+    (word, location) location is the starting byte position of the word.
+    """
+    word_list = []
+    wcurrent = []
+    windex = None
+    text = text.decode('latin-1')
+    for i, c in enumerate(text):
+        if c.isalnum():
+            wcurrent.append(c)
+            windex = i
+        elif wcurrent:
+            word = u''.join(wcurrent)
+            word_list.append((windex - len(word) + 1, word))
+            wcurrent = []
+
+    if wcurrent:
+        word = u''.join(wcurrent)
+        word_list.append((windex - len(word) + 1, word))
+
+    return word_list
+
+ 
+    cleaned_words = []
+    for index, word in words:
+        if len(word) < _WORD_MIN_LENGTH or word in _STOP_WORDS:
+            continue
+        cleaned_words.append((index, word))
+    return cleaned_words
+
+def words_normalize(words):
+    """
+    Do a normalization precess on words. In this case is just a tolower(),
+    but you can add accents stripping, convert to singular and so on...
+    """
+    normalized_words = []
+    for index, word in words:
+        wnormalized = word.lower()
+        normalized_words.append((index, wnormalized))
+    return normalized_words
+
+def word_index(text):
+    """
+    Just a helper method to process a text.
+    It calls word split, normalize and cleanup.
+    """
+    words = word_split(text)
+    words = words_normalize(words)
+    words = words_cleanup(words)
+    return words
+
+
+#have to save and learn this code to invert a dictionary and flip over#
+def inverted_index(text):
+    """
+    Create an Inverted-Index of the specified text document.
+        {word:[locations]}
+    """
+    inverted = {}
+
+    for index, word in word_index(text):
+        locations = inverted.setdefault(word, [])
+        locations.append(index)
+
+    return inverted
+
+def inverted_index_add(inverted, doc_id, doc_index):
+    """
+    Add Invertd-Index doc_index of the document doc_id to the 
+    Multi-Document Inverted-Index (inverted), 
+    using doc_id as document identifier.
+        {word:{doc_id:[locations]}}
+    """
+    for word, locations in doc_index.iteritems():
+        indices = inverted.setdefault(word, {})
+        indices[doc_id] = locations
+    return inverted
+
+def search(inverted, query):
+    """
+    Returns a set of documents id that contains all the words in your query.
+    """
+    words = [word for _, word in word_index(query) if word in inverted]
+    print words
+    results = [set(inverted[word].keys()) for word in words]
+    return reduce(lambda x, y: x & y, results) if results else []
+
+# Build Inverted-Index for documents
+inverted = {}
+chapter = list(range(1,239))
+chapter = ["Chapter " + str(f) for f in chapter]
+documents = dict(zip(chapter, book))
+
+for doc_id, text in documents.iteritems():
+    doc_index = inverted_index(text)
+    inverted_index_add(inverted, doc_id, doc_index)
+
+        
+# Print Inverted-Index
+#for word, doc_locations in inverted.iteritems():
+#    print word, doc_locations
+
+# Search something and print results
+final_result = search(inverted,'alexandrovna')
+print len(final_result)
+
+
+winnie = ['anna karenina','oblonsky']
+
+#if we want to print each print and its place 
+def listing_results(queries):
+    for query in queries:
+        result_docs = search(inverted, query)
+        print "Search for '%s': %r" % (query, result_docs)
+        for _, word in word_index(query):
+            def extract_text(doc, index): 
+                return documents[doc][index:index+40].replace('\n', ' ')
+    
+            for doc in result_docs:
+                for index in inverted[word][doc]:
+                    print '   - %s...' % extract_text(doc, index)
+        print
+
+listing_results(winnie)
+
+seconds = (time.time()-start)
+print 'It took', seconds, 'seconds.'
+print 'completed'
